b5898f3858d29d767797b92136666e36a0bf577b
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-04-19 11:57:40 -0400

    added error catching to productController

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,31 +7,34 @@ export const productController = {
     ProductModel
       .create(request.body)
       .then(data => response.json(data))
+      .catch((error) => console.error(error))
   },
   //define get route
   get(request, response) {
     ProductModel
       .find({})
       .then(data => response.json(data))
-
+      .catch((error) => console.error(error))
   },
   //define delete route
   delete(request, response) {
     ProductModel
       .deleteOne(request.params.id)
+      .catch((error) => console.error(error))
   },
 
   update(request, response) {
     ProductModel
       .findOneAndUpdate(request.params.id, request.body)
       .then(data => response.json(data))
+      .catch((error) => console.error(error))
 
   },
   getProductById(request, response) {
     ProductModel
       .findById(request.params.id)
       .then(data => response.json(data))
-
+      .catch((error) => console.error(error))
   }
 };
 
